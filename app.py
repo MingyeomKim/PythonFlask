e2128cc1d2ef0a0fa70280943a02901b1617c741
MGKIM01 <67851124+MGKIM01@users.noreply.github.com>
2021-01-22 01:46:59 +0900

    Comprise Get and Post method

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
 
 app = Flask(__name__)
-
-student_data = {
-    1:{"name":"김민겸","score": {"국어": 80, "수학" : 100}},
-    2:{"name":"김민주","score": {"국어" :100, "수학" :100}}
-}
+app.config["SECRET_KEY"] = '(복잡한 문자열)'
+#CSRF를 방지하기 위해 사용
 
 @app.route('/')
-def index():
-    return render_template("index.html", template_students = student_data)
-#render_template에서 index.html을 불러옴과 동시에 template_students 변수 정의
+def home():
+    return render_template('layout.html')
 
-@app.route("/student/<int:id>")
-def studnet(id):
-    return render_template("student.html",
-                           template_name = student_data[id]["name"],
-                           template_score = student_data[id]["score"])
+@app.route('/register',methods=["GET","POST"])
+#register URL을 회원가입으로 사용할 예정.
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():  #POST방식으로 요청한 경우
+        flash(f'{form.username.data}님 회원가입 완료!','success')
+        #부트스트랩으로 알림 메세지를 띄우기 위해 사용. (굳이 필요 x)
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form) #GET방식으로 요청한 경우
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
